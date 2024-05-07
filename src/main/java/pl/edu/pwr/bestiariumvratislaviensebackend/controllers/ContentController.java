d566d998db19add1b5a783a087602e25b52eb4c2
Kuba Bigaj <k.bigaj666@gmail.com>
2024-05-07 14:44:08 +0200

    Initial commit.

---
+++ src/main/java/pl/edu/pwr/bestiariumvratislaviensebackend/controllers/ContentController.java
@@ -0,0 +1,8 @@
+package pl.edu.pwr.bestiariumvratislaviensebackend.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ContentController {
+
+}
---
+++ src/test/java/pl/edu/pwr/bestiariumvratislaviensebackend/BestiariumVratislavienseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pwr.bestiariumvratislaviensebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestiariumVratislavienseBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
